a5b2be56522cd336a013a9fc609c235df76e83d1
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-17 15:18:08 +0100

    try to put bomb in square

--- js/css/js/script.js
+++ js/css/js/script.js
@@ -85,7 +85,7 @@ function play(e){
         square.addEventListener('click', function(){
             square.classList.add('safe');
         })
-        
         playground.appendChild(square);
+        
     }
 }
