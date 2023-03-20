2511a7f82ce6225607da883a659fe5052eafe110
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-20 14:54:18 +0100

    ADD reveal bomb

--- js/css/js/script.js
+++ js/css/js/script.js
@@ -1,116 +1,106 @@
-/*`
-<div class="square"> 1 </div>
-easy = 100 quadrati
-medium 81 quadrati
-hard 49 quadrati
 
-    width: calc(100% / 10);
-    height: calc(100% / 10) ;
-`*/
 
-const formLevel = document.getElementById('formLevel');
-formLevel.addEventListener('submit', play);
+const formLevel = document.getElementById("formLevel");
+formLevel.addEventListener("submit", play);
 var counterVal = 0;
 
-function incrementClick() {
-    updateDisplay(++counterVal);
-}
-
-function resetCounter() {
-    counterVal = 0;
-    updateDisplay(counterVal);
-}
-
-function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
-}
 //  FUNZIONE PER CREARE CELLE
 
-
-function drawSquare(index, numSquare ){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `calc(100% / ${numSquare}) `
-    square.style.height = square.style.width;
-    square.innerHTML = index;
-    return square;
-
+function drawSquare(index, numSquare) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.style.width = `calc(100% / ${numSquare}) `;
+  square.style.height = square.style.width;
+  square.innerHTML = index;
+  return square;
 }
-
 
 //  FUNZIONE PER GENERARE L'ARRAY DELLE BOMBE
 
-function generateBombs(numBomb, numSquare){
-    const bombs = [];
-    while (bombs.length < numBomb){
-       const bomb =  getRndNumber(1, numSquare);
-       if (bombs.indexOf(bomb) === -1){
-        bombs.push(bomb);
-       }
-    } 
-    return bombs;
-
+function generateBombs(numBomb, numSquare) {
+  const bombs = [];
+  while (bombs.length < numBomb) {
+    const bomb = getRndNumber(1, numSquare);
+    if (bombs.indexOf(bomb) === -1) {
+      bombs.push(bomb);
+    }
+  }
+  return bombs;
+}
+function showAllBombs(bombs){
+    const squares = document.querySelectorAll('.square');
+    for(let square of squares ){
+        if (bombs.includes(parseInt(square.innerText))){
+            square.classList.add('unsafe');
+        }
+    }
 }
 
-
-
-
 //  FUNZIONE PER FAR PARTIRE IL GIOCO
-function play(e){
-    e.preventDefault();
-    const NUM_BOMBS = 16;
-    const playground = document.getElementById('playground');
-    playground.innerHTML = ' ';
-    const numBombs = 16;
-    //  selezione livello 
-    const level = document.getElementById('level').value;
- 
-    let squareNmr;
-   
-    switch(level){
-        case 'easy':
-            squareNmr = 100;
-            break
-            case 'medium':
-            squareNmr = 81;
-            break
-            case 'hard':
-                squareNmr = 49;
-                break;
-        
-    };
+function play(e) {
+  e.preventDefault();
+  const NUM_BOMBS = 16;
+  const playground = document.getElementById("playground");
+  playground.innerHTML = " ";
+  const numBombs = 16;
+  let gameOver = false;
+  //  selezione livello
+  const level = document.getElementById("level").value;
+
+  let squareNmr;
+
+  switch (level) {
+    case "easy":
+      squareNmr = 100;
+      break;
+    case "medium":
+      squareNmr = 81;
+      break;
+    case "hard":
+      squareNmr = 49;
+      break;
+  }
+
+  // celle per lato
+
+  let squareRow = Math.sqrt(squareNmr);
+
+  const bombs = generateBombs(NUM_BOMBS, squareNmr);
+  console.log(bombs);
+  let maxScore = squareNmr - numBombs;
+
+  // num celle creo la cella con funzione drawSquare
+
+  for (let i = 1; i <= squareNmr; i++) {
+    const square = drawSquare(i, squareRow);
+    const contentSquare = parseInt(square.innerText);
+
+    square.addEventListener("click", function () {
+        if(!gameOver){
+            if (bombs.includes(contentSquare)) {
+                square.classList.add("unsafe");
+                alert("Hai cliccato su una bomba, HAI PERSO !!");
+                gameOver = true;
+                showAllBombs(bombs);
+              } else {
+                square.classList.add("safe");
+              } 
+        }
     
-    // celle per lato
+    });
+    playground.appendChild(square);
+  }
+}
+var counterVal = 0;
+function incrementClick() {
+  updateDisplay(++counterVal);
+}
 
+function resetCounter() {
+  counterVal = 0;
+  updateDisplay(counterVal);
+}
 
-    let squareRow =  Math.sqrt(squareNmr);
-
-     const bombs = generateBombs(NUM_BOMBS , squareNmr);
-     console.log(bombs);
-
-
-    // num celle creo la cella con funzione drawSquare
-
-
-    for(let i = 1; i <= squareNmr; i++ ){
-        const  square = drawSquare(i,squareRow );
-        const contentSquare = parseInt(square.innerText);
-  
-        square.addEventListener('click', function(){
-            
-            if (bombs.includes(contentSquare)){
-                square.classList.add('unsafe');
-                alert('Hai cliccato su una bomba, HAI PERSO !!');
-                
-            } else {
-                square.classList.add('safe');
-            }
-            console.log(contentSquare);
-
-        })
-        playground.appendChild(square);
-    }
-
-
-
+function updateDisplay(val) {
+  document.getElementById("counter-label").innerHTML = val;
 }
